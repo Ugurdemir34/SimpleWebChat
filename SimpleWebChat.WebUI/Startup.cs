7cf357c196e38479f55080678bfea06799fdc625
ugurdemir <ugurdemir551@gmail.com>
2021-02-16 02:09:05 +0300

    Authorize and Dependencies Added

--- SimpleWebChat.WebUI/Startup.cs
+++ SimpleWebChat.WebUI/Startup.cs
@@ -2,13 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SimpleWebChat.Business.Abstract;
+using SimpleWebChat.Business.Concrete;
 using SimpleWebChat.DataAccess.Abstract;
+using SimpleWebChat.DataAccess.Concrete.EntityFramework;
 
 namespace SimpleWebChat.WebUI
 {
@@ -24,8 +29,36 @@ namespace SimpleWebChat.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Auth/Login";
+                options.LogoutPath = "/Login/Logout";
+                options.AccessDeniedPath = "/Account/AccessDenied";
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+                options.Cookie = new Microsoft.AspNetCore.Http.CookieBuilder
+                {
+                    HttpOnly = true,
+                    Name = "Sinav.Cookie",
+                    Path = "/",
+                    SameSite = SameSiteMode.Lax,
+                    SecurePolicy = CookieSecurePolicy.SameAsRequest
+                };
+            });
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+           .AddCookie(options =>
+           {
+               options.LoginPath = "/Auth/Login/";
+               options.LogoutPath = "/Auth/Logout/";
+               options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+           });
             services.AddControllersWithViews();
-            
+            services.AddSession();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IUserDal, EfUserDal>();
+            services.AddScoped<IUserService, UserManager>();
+            services.AddDistributedMemoryCache();
+            services.AddHttpContextAccessor();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +74,8 @@ namespace SimpleWebChat.WebUI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseSession();
+            app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -52,7 +87,7 @@ namespace SimpleWebChat.WebUI
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
