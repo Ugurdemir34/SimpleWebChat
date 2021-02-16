b0cb5b4fc79eaf1afc7727747c384f1eae9ffa32
ugurdemir <ugurdemir551@gmail.com>
2021-02-16 19:19:13 +0300

    Get Messages function created

---
+++ SimpleWebChat.Business/Abstract/IMessageService.cs
@@ -0,0 +1,13 @@
+﻿using SimpleWebChat.Entities.Concrete;
+using SimpleWebChat.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleWebChat.Business.Abstract
+{
+    public interface IMessageService
+    {
+        List<UserFriendMessageListDto> GetMessages(int userid,int otheruserid);
+    }
+}
---
+++ SimpleWebChat.Business/Concrete/MessageManager.cs
@@ -0,0 +1,23 @@
+﻿using SimpleWebChat.Business.Abstract;
+using SimpleWebChat.DataAccess.Abstract;
+using SimpleWebChat.Entities.Concrete;
+using SimpleWebChat.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleWebChat.Business.Concrete
+{
+    public class MessageManager : IMessageService
+    {
+        private IMessageDal _messageDal;
+        public MessageManager(IMessageDal messageDal)
+        {
+            _messageDal = messageDal;
+        }
+        public List<UserFriendMessageListDto> GetMessages(int userid, int otheruserid)
+        {
+           return _messageDal.GetConversation(userid, otheruserid);
+        }
+    }
+}
