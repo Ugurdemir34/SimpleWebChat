16e74ad9e97d9beefe66a077219a6fa83716a011
ugurdemir <ugurdemir551@gmail.com>
2021-02-16 19:22:55 +0300

    Conversation view component added

---
+++ SimpleWebChat.WebUI/ViewComponents/ConversationViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using SimpleWebChat.Business.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleWebChat.WebUI.ViewComponents
+{
+    public class ConversationViewComponent:ViewComponent
+    {
+        private IMessageService _messageService;
+        public ConversationViewComponent(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+        public ViewViewComponentResult Invoke(int userid,int otheruserid)
+        {
+            var usermessage = _messageService.GetMessages(userid, otheruserid);
+            return View(usermessage);
+        }
+    }
+}
