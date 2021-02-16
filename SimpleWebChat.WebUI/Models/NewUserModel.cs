977719db243c566ddbd2f1676becc8ef55046af0
ugurdemir <ugurdemir551@gmail.com>
2021-02-16 14:19:23 +0300

    User add model created

---
+++ SimpleWebChat.WebUI/Models/NewUserModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleWebChat.WebUI.Models
+{
+    public class NewUserModel
+    {
+        
+        [Required]
+        [DisplayName("Username")]
+        public string Username { get; set; }
+        [Required]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+        [Required]
+        [DisplayName("Surname")]
+        public string Surname { get; set; }
+        [Required]
+        [DisplayName("Email")]
+        public string Email { get; set; }
+        [Required]
+        [DisplayName("Password")]
+        public string Password { get; set; }
+        [DisplayName("Admin ")]
+        public bool IsAdmin { get; set; }
+    }
+}
