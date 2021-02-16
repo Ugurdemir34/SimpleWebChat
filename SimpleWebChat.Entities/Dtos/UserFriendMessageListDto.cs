5a447e9c2b56c1c019fb999d53ccf9d6d62a3d2f
ugurdemir <ugurdemir551@gmail.com>
2021-02-16 19:19:54 +0300

    User Friend Model created

---
+++ SimpleWebChat.Entities/Dtos/UserFriendMessageListDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleWebChat.Entities.Dtos
+{
+    public class UserFriendMessageListDto
+    {
+        public int UserId { get; set; }
+        public int FriendId { get; set; }
+        public string MessageContent { get; set; }
+        public DateTime DeliveredDate { get; set; }
+     
+    }
+}
