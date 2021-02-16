3697c2409895d9231895fbe6b7cdb4fea5127236
ugurdemir <ugurdemir551@gmail.com>
2021-02-16 19:19:25 +0300

    Message Dal Created

---
+++ SimpleWebChat.DataAccess/Abstract/IMessageDal.cs
@@ -0,0 +1,14 @@
+﻿using SimpleWebChat.Core.DataAccess;
+using SimpleWebChat.Entities.Concrete;
+using SimpleWebChat.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleWebChat.DataAccess.Abstract
+{
+    public interface IMessageDal : IEntityRepository<Message>
+    {
+        List<UserFriendMessageListDto> GetConversation(int userid, int otheruserid);
+    }
+}
---
+++ SimpleWebChat.DataAccess/Concrete/EntityFramework/EfMessageDal.cs
@@ -0,0 +1,34 @@
+﻿using SimpleWebChat.Core.DataAccess.EntityFramework;
+using SimpleWebChat.DataAccess.Abstract;
+using SimpleWebChat.DataAccess.Concrete.EntityFramework.Contexts;
+using SimpleWebChat.Entities.Concrete;
+using SimpleWebChat.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleWebChat.DataAccess.Concrete.EntityFramework
+{
+    public class EfMessageDal : EfEntityRepositoryBase<Message, ChatContext>, IMessageDal
+    {
+        public List<UserFriendMessageListDto> GetConversation(int userid, int otheruserid)
+        {
+            using (var _context = new ChatContext())
+            {
+                var result = new List<UserFriendMessageListDto>();
+               
+                result = _context.Messages.Where(i => i.MessageFrom == userid && i.MessageTo == otheruserid || i.MessageFrom == otheruserid && i.MessageTo == userid)
+                                          .Select(i => new UserFriendMessageListDto()
+                                          {
+                                              DeliveredDate = i.DeliveredDate,
+                                              FriendId = i.MessageTo,
+                                              UserId = i.MessageFrom,
+                                              MessageContent = i.MessageContent
+                                          }).ToList();
+                // }
+                return result;
+            }
+        }
+    }
+}
