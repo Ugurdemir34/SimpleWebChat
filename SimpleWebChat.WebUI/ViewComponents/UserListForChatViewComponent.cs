91817afb2034d5307cf504029c83a2dfc1b20aed
ugurdemir <ugurdemir551@gmail.com>
2021-02-16 19:22:46 +0300

    User list view component added

---
+++ SimpleWebChat.WebUI/ViewComponents/UserListForChatViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using SimpleWebChat.Business.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleWebChat.WebUI.ViewComponents
+{
+    public class UserListForChatViewComponent:ViewComponent
+    {
+        private IUserService _userService;
+        public UserListForChatViewComponent(IUserService userService)
+        {
+            _userService = userService;
+        }
+        public ViewViewComponentResult Invoke()
+        {
+            var users = _userService.GetUserList().OrderBy(a => a.Name).ToList();
+            return View(users);
+        }
+    }
+}
