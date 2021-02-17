12714e0e5d6d9832535452f3adf62153f2363b05
ugurdemir <ugurdemir551@gmail.com>
2021-02-18 02:09:41 +0300

    Model for update created

---
+++ SimpleWebChat.WebUI/Models/UpdateUserModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleWebChat.WebUI.Models
+{
+    public class UpdateUserModel
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
